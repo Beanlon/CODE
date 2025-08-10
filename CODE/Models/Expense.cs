8efe77ae3d1400716212ac5f924a59d05e8c1f66
Your Name <you@example.com>
2025-08-10 22:02:16 +0800

    Add project files.

---
+++ CODE/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trial.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [Required]
+
+        public string? Description { get; set; }
+    }
+}
